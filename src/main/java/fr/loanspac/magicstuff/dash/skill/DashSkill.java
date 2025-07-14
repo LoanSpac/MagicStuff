d63014cbf3de25455a33f34059815da37e7df8e2
LoanSpac <scp04912@hotmail.com>
2025-07-14 15:28:20 +0200

    feat/sword Add cooldown in skill

--- src/main/java/fr/loanspac/magicstuff/dash/skill/DashSkill.java
+++ src/main/java/fr/loanspac/magicstuff/dash/skill/DashSkill.java
@@ -1,8 +1,11 @@
 package fr.loanspac.magicstuff.dash.skill;
 
 import fr.loanspac.magicstuff.sword.skill.Skill;
+import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
+
+import java.time.Duration;
 
 public class DashSkill extends Skill {
 
@@ -12,6 +15,12 @@ public class DashSkill extends Skill {
 
     @Override
     public void executor(Player player) {
+        if (this.getCooldown().hasCooldown(player.getUniqueId())) {
+            player.sendMessage(Component.text("§cYour dash is in cooldown !"));
+            player.sendMessage(Component.text("§cPlease wait " + this.getCooldown().getRemainingCooldown(player.getUniqueId()).getSeconds() + " seconds"));
+            return;
+        }
+        this.getCooldown().setCooldown(player.getUniqueId(), Duration.ofSeconds(5));
         Vector direction = player.getEyeLocation().getDirection();
         direction.multiply(15);
         direction.normalize();
--- src/main/java/fr/loanspac/magicstuff/sword/skill/Skill.java
+++ src/main/java/fr/loanspac/magicstuff/sword/skill/Skill.java
@@ -1,11 +1,13 @@
 package fr.loanspac.magicstuff.sword.skill;
 
+import fr.loanspac.magicstuff.utils.CooldownManager;
 import lombok.Getter;
 import org.bukkit.entity.Player;
 
 @Getter
 public abstract class Skill {
     private final String name;
+    private final CooldownManager cooldown = new CooldownManager();
     public abstract void executor(Player player);
 
     protected Skill(String name) {
